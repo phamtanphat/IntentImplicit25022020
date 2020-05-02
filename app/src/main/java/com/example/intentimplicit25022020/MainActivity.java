7bd9cda819e7d919c044127cac233ac786fb792c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-05-02 21:14:07 +0700

    gallery

--- app/src/main/java/com/example/intentimplicit25022020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit25022020/MainActivity.java
@@ -9,12 +9,16 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.security.Permissions;
 import java.security.acl.Permission;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     Button mBtnCamera,mBtnGallery;
     ImageView mImg;
     int REQUEST_CODE_CAMERA = 1;
+    int REQUEST_CODE_GALLERY = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +46,16 @@ public class MainActivity extends AppCompatActivity {
                         );
             }
         });
+        mBtnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
     }
 
     @Override
@@ -51,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,REQUEST_CODE_CAMERA);
             }
         }
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,REQUEST_CODE_GALLERY);
+            }
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
@@ -59,6 +81,18 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null){
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             mImg.setImageBitmap(bitmap);
+        }
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+//            mImg.setImageURI(uri);
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                mImg.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
